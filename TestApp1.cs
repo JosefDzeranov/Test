67c61f3d2ce04d510532c7fac9eb03f48210d705
Ktvfylh <tarshsergey@yandex.ru>
2016-05-13 00:21:57 +0300

    Add files via upload

---
+++ TestApp1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp1
+{
+    class Program
+    {
+        private struct Node
+        {
+            public string Current;
+            public string Next;
+        }
+        static void Main(string[] args)
+        {
+            List<Node> list = getNodes();
+            List<Node> sortedList = new List<Node>();
+            list.ForEach(node => Console.WriteLine("{0} > {1}", node.Current, node.Next)); Console.WriteLine();
+            
+            Stopwatch s = Stopwatch.StartNew();
+            Node head = getHeadNode(list);
+            list.Remove(head);
+            sortedList.Add(head);
+            while (list.Count > 0)
+            {
+                sortedList.Add(list.Find(node => node.Current == sortedList.Last().Next));
+                list.Remove(sortedList.Last());
+            }
+            s.Stop();
+
+            sortedList.ForEach(node => Console.WriteLine("{0} > {1}", node.Current, node.Next));
+            Console.WriteLine("Elapsed Time: {0} ms", s.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+        private static Node getHeadNode(List<Node> list)
+        {
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+            for (int i = 0; i < list.Count(); i++)
+            {
+                if (counts.ContainsKey(list[i].Current))
+                    counts[list[i].Current] = 2;
+                else
+                    counts.Add(list[i].Current, 1);
+
+                if (counts.ContainsKey(list[i].Next))
+                    counts[list[i].Next] = 2;
+                else
+                    counts.Add(list[i].Next, 1);
+            }
+            return list.Find(node => counts.Where(x => x.Value == 1).Select(x => x.Key).Any(x => x == node.Current));
+        }
+        private static List<Node> getNodes()
+        {
+            Random rand = new Random();
+            List<Node> list = new List<Node>();
+            List<string> cities = new List<string>() { "Мельбурн", "Кельн", "Москва", "Париж", "Вашингтон", "Франкфурт", "Токио", "Дрезден", "Бостон", "Хьюстон", "Мадрид" };
+
+            int counter = rand.Next(0, cities.Count - 1);
+            string Prev = cities[counter];
+            cities.RemoveAt(counter);
+
+            while (cities.Count > 0)
+            {
+                counter = rand.Next(0, cities.Count - 1);
+                list.Add(new Node { Current = Prev, Next = cities[counter] });
+                Prev = cities[counter];
+                cities.RemoveAt(counter);
+            }
+            list.Shuffle();
+
+            return list;
+        }
+    }
+    public static class Extensions
+    {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            Random rand = new Random();
+            int count = list.Count;
+            while (count > 1)
+            {
+                count--;
+                int position = rand.Next(count + 1);
+                T value = list[position];
+                list[position] = list[count];
+                list[count] = value;
+            }
+        }
+    }  
+}
---
+++ TestApp2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp2
+{
+    class Program
+    {
+        private struct Sale
+        {
+            public Guid salesid;
+            public string productid;
+            public DateTime datetime;
+            public string customerid;
+        }
+        static void Main(string[] args)
+        {
+            List<Sale> list = getSales();
+            Stopwatch s = Stopwatch.StartNew();
+            var productsRates = list.GroupBy(sale => sale.customerid, sale => sale, (key, value) => new { customerid = key, productid = value.OrderBy(sale => sale.datetime).First().productid })
+                                .GroupBy(x => x.productid, x => x.customerid, (key, value) => new { productid = key, rate = value.Count()});
+            s.Stop();
+            foreach (var productRate in productsRates)
+                Console.WriteLine(productRate.productid + " " + productRate.rate);
+            Console.WriteLine("Elapsed Time: {0} ms", s.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+        private static List<Sale> getSales()
+        {
+            Random rand = new Random();
+            List<string> products = new List<string>() { "Хлеб", "Молоко", "Мясо"};
+            List<string> customers = new List<string>() { "Петров", "Иванов", "Сидоров", "Соколов", "Мельников", "Стрельников", "Степанов" };
+            List<Sale> list = new List<Sale>();
+            DateTime start = new DateTime(2016, 1, 1);
+            int range = (DateTime.Today - start).Days;
+            while (list.Count < 100)
+            {
+                list.Add(new Sale
+                {
+                    salesid = Guid.NewGuid(),
+                    productid = products[rand.Next(products.Count)],
+                    datetime = start.AddDays(rand.Next(range)),
+                    customerid = customers[rand.Next(customers.Count)]
+                });
+                Console.WriteLine("{0} {1} {2} {3}", list.Last().salesid, list.Last().productid, list.Last().datetime.ToString("d"), list.Last().customerid);
+            }
+            return list;
+        }
+    }
+}
